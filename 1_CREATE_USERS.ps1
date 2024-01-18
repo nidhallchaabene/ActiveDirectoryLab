ff4d78f0d8f429d87ba100d99c056548fe7b9761
nidhallchaabene <nidhall.chaabene@gmail.com>
2024-01-18 22:34:09 +0100

    AD lab

---
+++ 1_CREATE_USERS.ps1
@@ -0,0 +1,24 @@
+﻿
+$PASSWORD_FOR_USERS   = "Password1"
+$USER_FIRST_LAST_LIST = Get-Content .\names.txt
+# ------------------------------------------------------ #
+
+$password = ConvertTo-SecureString $PASSWORD_FOR_USERS -AsPlainText -Force
+New-ADOrganizationalUnit -Name _USERS -ProtectedFromAccidentalDeletion $false
+
+foreach ($n in $USER_FIRST_LAST_LIST) {
+    $first = $n.Split(" ")[0].ToLower() <# this line injects the first name of the user into the variable first (Ex: nidhal) #>
+    $last = $n.Split(" ")[1].ToLower() <# this line injects the last name of the user into the variable last (Ex: chaabene) #>
+    $username = "$($first.Substring(0,1))$($last)".ToLower() <# this line combines the first letter of the first name of the user with last name (Ex: nchaabene) #>
+    Write-Host "Creating user: $($username)" -BackgroundColor Black -ForegroundColor Cyan
+    
+    New-AdUser -AccountPassword $password `
+               -GivenName $first `
+               -Surname $last `
+               -DisplayName $username `
+               -Name $username `
+               -EmployeeID $username `
+               -PasswordNeverExpires $true `
+               -Path "ou=_USERS,$(([ADSI]`"").distinguishedName)" `
+               -Enabled $true
+}
