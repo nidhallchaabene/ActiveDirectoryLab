cec57ba478f195320a5867a221aba058436033d8
nidhallchaabene <nidhall.chaabene@gmail.com>
2024-01-18 23:09:40 +0100

    AD lab

--- 1_CREATE_USERS.ps1
+++ 1_CREATE_USERS.ps1
@@ -9,7 +9,7 @@ New-ADOrganizationalUnit -Name _USERS -ProtectedFromAccidentalDeletion $false
 foreach ($n in $USER_FIRST_LAST_LIST) {
     $first = $n.Split(" ")[0].ToLower() <# this line injects the first name of the user into the variable first (Ex: nidhal) #>
     $last = $n.Split(" ")[1].ToLower() <# this line injects the last name of the user into the variable last (Ex: chaabene) #>
-    $username = "$($first.Substring(0,1))$($last)".ToLower() <# this line combines the first letter of the first name of the user with last name (Ex: nchaabene) #>
+    $username = "$($first.Substring(0,1))$($last)".ToLower() <# this line combines the first letter of the first name with the last name of the user (Ex: nchaabene) #>
     Write-Host "Creating user: $($username)" -BackgroundColor Black -ForegroundColor Cyan
     
     New-AdUser -AccountPassword $password `
